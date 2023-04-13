fccd50ab6f9ecc3f5a33f0c05d59a4139ca02522
BTL86 <phamlehuy52@gmail.com>
2023-04-13 16:31:21 +0700

    font icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Karla: ["Karla", ",sans-serif"],
+      },
+      colors: {
+        "light-coffee": "#C89f94",
+      },
+    },
   },
   plugins: [],
 };
