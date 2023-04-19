5ffd738d297719dbf5d60f54e515138c4295d74f
BTL86 <phamlehuy52@gmail.com>
2023-04-19 16:34:06 +0700

    tmp

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {
       colors: {
         "light-coffee": "#C89f94",
+        coffee: {
+          50: "#E8D6D0",
+          200: "#C89F94",
+          400: "#A25F4B",
+          600: "#744838",
+        },
       },
       keyframes: {
         slideDown: {
